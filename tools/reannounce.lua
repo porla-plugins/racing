b5aebe649d147dae3fc60ea3e710946ec2317e47
l1meju1ce <129756944+l1meju1ce@users.noreply.github.com>
2023-08-29 18:43:24 +0200

    Added max_age, add_tags and remove_tags. Fixed a bug where an active reannounce timer without a valid torrent (removed) would loop forever.

--- tools/reannounce.lua
+++ tools/reannounce.lua
@@ -5,7 +5,8 @@ local torrents = require("torrents")
 local active_timers = {}
 
 return {
-    begin = function(torrent, interval, max_tries)
+    begin = function(torrent, interval, max_tries, max_age, add_tags, remove_tags)
+
         local torrentstatus = torrent:status()
         local peers         = torrentstatus.num_peers
         local name          = torrentstatus.name
@@ -18,6 +19,15 @@ return {
         local timer = timers.new({
             interval = interval,
             callback = function()
+                if not torrent:is_valid() then
+                    log.info("Torrent doesn't exist - not reannouncing")
+
+                    active_timers[name].timer:cancel()
+                    active_timers[name] = nil
+
+                    return
+                end
+
                 log.debug(string.format("Checking if %s needs reannouncing", name))
 
                 local torrentstatus = torrent:status()
@@ -33,11 +43,33 @@ return {
                     return
                 end
 
-                if active_timers[name].tries >= max_tries then
-                    log.info(string.format("Torrent %s reached max announce tries", name))
+                local age = os.time() - torrentstatus.added_time
+
+                if (active_timers[name].tries >= max_tries) or (age > max_age) then
+                    log.info(string.format("Torrent %s reached max announce tries or age", name))
+                    local userdata = torrent:userdata()
+
+                    for _,v in pairs(add_tags) do
+                        userdata.tags:add(v)
+                    end
+
+                    for _,v in pairs(remove_tags) do
+                        userdata.tags:erase(v)
+                    end
+
+                    torrent:set_flags({
+                        auto_managed = false,
+                    })
+                    torrent:pause()
 
                     active_timers[name].timer:cancel()
                     active_timers[name] = nil
+
+                    return
+                end
+
+                if torrent:flags().paused then
+                    log.info(string.format("Torrent %s paused, not reannouncing", name))
 
                     return
                 end
